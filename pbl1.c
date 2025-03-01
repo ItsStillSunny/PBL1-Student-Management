e3ada60846d94fa7746cbf31e753e49f20428711
ItsStillSunny <huynhhieunghiadn@gmail.com>
2025-03-01 20:39:36 +0700

    Add files via upload

---
+++ pbl1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct SinhVien
+{
+    char HoLot[20];
+    char Ten[10];
+    char GioiTinh[3];
+    char NgaySinh[10];
+    char DiaChi[50];
+} SinhVien;
+
+void ThemSinhVien()
+{
+
+}
+
+void XoaSinhVien()
+{
+
+}
+
+void SapXepSinhVien()
+{
+
+}
+
+void TimSinhVien()
+{
+
+}
+
+void CapMaSinhVien()
+{
+
+}
+
+void CapEmail()
+{
+
+}
+
+void XuatDanhSachSinhVien()
+{
+
+}
